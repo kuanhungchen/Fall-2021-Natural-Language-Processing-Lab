4d7916a311843f8eba2aa8d179ef0e6bce73e493
knhngchn <khchen.me@gmail.com>
2021-10-14 17:00:39 +0800

    feat: add mapper

--- week05/mapper.py
+++ week05/mapper.py
@@ -1,24 +1,33 @@
+import re
 import sys
+
+MAX_DIST = 5
 
 def preprocess(text):
     # preprocess/tokenize the sentence
-    processed_data = ...
-    return processed_data
+    text = text.lower()
+    tokens = re.split('\W+', text)
+    return [token for token in tokens if token != ""]
 
 def _map(text: str):
     tokens = preprocess(text)
 
     # [ TODO ] generate the mapper output
     # Output: "{pivot}\t{word}\t{distance}\t{count}"
-    # Example: 
+    # Example:
     #   predict is  -3  1
     #   predict used    -2  1
     #   predict the -1  1
     #   predict the 1   1
     #   ...
-    ...
-    
+    N = len(tokens)
+    for i, pivot in enumerate(tokens):
+        for (word, distance) in [(tokens[j], j - i) for j in range(max(0, i - MAX_DIST), min(N, i + MAX_DIST + 1))]:
+            if distance == 0:
+                continue
+            print("{}\t{}\t{}\t{}".format(pivot, word, distance, 1))
+
 if __name__== "__main__" :
     for line in sys.stdin:
         _map(line)
-     
+        break
